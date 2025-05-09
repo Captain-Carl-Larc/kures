fd7baf6cf236c1a8088d8e8ff550c829a2ecd242
Captain-Carl-Larc <155651529+Captain-Carl-Larc@users.noreply.github.com>
2025-05-09 21:03:19 +0300

    data fetching done

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,50 @@
+"use client"
+import { useEffect, useState } from 'react';
+import supabase from './config/supabaseClient';
 
-import supabase from "./config/supabaseClient"
+// Define a type for the user data
+interface User {
+  id: number;
+  name: string;
+  age:number
+  // Add other properties as needed, based on your database schema
+}
 
+const Home = () => {
+  const [fetchError, setFetchError] = useState<string | null>(null);
+  const [users, setUsers] = useState<User[] | null>(null);
 
-export default function Home() {
-  console.log('supabase')
-  console.log(supabase)
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const { data, error } = await supabase
+        .from('users')
+        .select();
+
+      if (error) {
+        setFetchError('Could not fetch the users');
+        setUsers(null);
+      }
+      if (data) {
+        setUsers(data);
+        setFetchError(null);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1 className="font-bold text-3xl">
-        Home page
-      </h1>
+    <div className="page home">
+      {fetchError && <p>{fetchError}</p>}
+      {users && (
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>{user.name}<span className='px-4'>{user.age}</span></li>
+          ))}
+        </ul>
+      )}
     </div>
   );
-}
+};
+
+export default Home;
