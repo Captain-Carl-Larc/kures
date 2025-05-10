9c7e7fca1b4cc540d4b5bc6f50ee7b425a1673d6
Captain-Carl-Larc <155651529+Captain-Carl-Larc@users.noreply.github.com>
2025-05-10 10:21:01 +0300

    update home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,50 +1,59 @@
-"use client"
-import { useEffect, useState } from 'react';
-import supabase from './config/supabaseClient';
-
-// Define a type for the user data
-interface User {
-  id: number;
-  name: string;
-  age:number
-  // Add other properties as needed, based on your database schema
+function Home() {
+  return ( 
+    <div>
+      <h1 className="font-bold text-3xl">Home</h1>
+    </div>
+   );
 }
 
-const Home = () => {
-  const [fetchError, setFetchError] = useState<string | null>(null);
-  const [users, setUsers] = useState<User[] | null>(null);
-
-  useEffect(() => {
-    const fetchUsers = async () => {
-      const { data, error } = await supabase
-        .from('users')
-        .select();
-
-      if (error) {
-        setFetchError('Could not fetch the users');
-        setUsers(null);
-      }
-      if (data) {
-        setUsers(data);
-        setFetchError(null);
-      }
-    };
-
-    fetchUsers();
-  }, []);
-
-  return (
-    <div className="page home">
-      {fetchError && <p>{fetchError}</p>}
-      {users && (
-        <ul>
-          {users.map((user) => (
-            <li key={user.id} className='text-2xl font-semibold'>{user.name}<span className='px-4'>{user.age}</span></li>
-          ))}
-        </ul>
-      )}
-    </div>
-  );
-};
-
 export default Home;
+// "use client"
+// import { useEffect, useState } from 'react';
+// import supabase from '../config/supabaseClient';
+
+// // Define a type for the user data
+// interface User {
+//   id: number;
+//   name: string;
+//   age:number
+//   // Add other properties as needed, based on your database schema
+// }
+
+// const Home = () => {
+//   const [fetchError, setFetchError] = useState<string | null>(null);
+//   const [users, setUsers] = useState<User[] | null>(null);
+
+//   useEffect(() => {
+//     const fetchUsers = async () => {
+//       const { data, error } = await supabase
+//         .from('users')
+//         .select();
+
+//       if (error) {
+//         setFetchError('Could not fetch the users');
+//         setUsers(null);
+//       }
+//       if (data) {
+//         setUsers(data);
+//         setFetchError(null);
+//       }
+//     };
+
+//     fetchUsers();
+//   }, []);
+
+//   return (
+//     <div className="page home">
+//       {fetchError && <p>{fetchError}</p>}
+//       {users && (
+//         <ul>
+//           {users.map((user) => (
+//             <li key={user.id} className='text-2xl font-semibold'>{user.name}<span className='px-4'>{user.age}</span></li>
+//           ))}
+//         </ul>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Home;
