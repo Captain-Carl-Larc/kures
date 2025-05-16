23fff807cf8feb668ca8078cd98e94bdc671e58c
Captain-Carl-Larc <155651529+Captain-Carl-Larc@users.noreply.github.com>
2025-05-16 10:12:51 +0300

    bav and footer added in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,33 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <header>
+          <nav className="flex items-center justify-between p-4 bg-gray-800 text-white">
+            <h1 className="text-3xl font-bold">
+              Kures
+            </h1>
+            <ul className="flex space-x-4">
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/about">About</a>
+              </li>
+              <li>
+                <a href="/contact">Contact</a>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main>
+          {children}
+        </main>
+        <footer className="flex items-center justify-center p-4 bg-gray-800 text-white">
+          <p>
+            &copy; {new Date().getFullYear()} Kures. All rights reserved.
+            <br />
+          </p>
+        </footer>
       </body>
     </html>
   );
