d3a163594063a3b2c4d20dbc48ad6510744e17ee
Captain-Carl-Larc <155651529+Captain-Carl-Larc@users.noreply.github.com>
2025-05-16 10:17:08 +0300

    dashboard sidebar styling
    added cursor-pointer  style

--- app/user/dashboard/page.jsx
+++ app/user/dashboard/page.jsx
@@ -4,36 +4,36 @@ import { faHouse,faGear,faUser,faCirclePlus,faBook } from '@fortawesome/free-sol
 function Dashboard() {
     return ( 
         <div className='bg-gray-200 h-screen'>
-            <h1 className="font-bold text-3xl text-center">
+            <h1 className="font-bold text-3xl text-black text-center">
                 Dashboard    
             </h1>
             <div className='grid grid-cols-12 h-[calc(100vh-3rem)]'>
                 <section id='dashboard' className='bg-gray-400  h-full flex flex-col gap-y-4 p-4 col-span-2'>
-                    <div className="flex justify-start items-center gap-2 ">
+                    <div className="flex justify-start items-center gap-2 cursor-pointer">
                         <FontAwesomeIcon icon={faHouse} className='text-white h-6 w-6' />
                         <h4>
                             Home
                         </h4>
                     </div>               
-                    <div className="flex justify-start items-center gap-2 ">
+                    <div className="flex justify-start items-center cursor-pointer gap-2 ">
                         <FontAwesomeIcon icon={faUser} className='text-white h-6 w-6' />
                         <h4>
                             Profile
                         </h4>
                     </div>               
-                    <div className="flex justify-start items-center gap-2 ">
+                    <div className="flex justify-start items-center cursor-pointer gap-2 ">
                         <FontAwesomeIcon icon={faGear} className='text-white h-6 w-6' />                    
                         <h4>
                             Settings
                         </h4>
                     </div>         
-                    <div className="flex justify-start items-center gap-2 ">
+                    <div className="flex justify-start items-center cursor-pointer gap-2 ">
                         <FontAwesomeIcon icon={faBook} className='text-white h-6 w-6' />
                         <h4>
                             Stories
                         </h4>
                     </div>               
-                    <div className="flex justify-start items-center gap-2 ">
+                    <div className="flex justify-start items-center cursor-pointer gap-2 ">
                         <FontAwesomeIcon icon={faCirclePlus} className='text-white h-6 w-6' />
                         <h4>
                             Stories
