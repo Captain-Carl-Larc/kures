1712b0de9c167c576752563c86f4f5d123afc460
Captain-Carl-Larc <155651529+Captain-Carl-Larc@users.noreply.github.com>
2025-05-16 10:13:05 +0300

    dashboard layout made

--- app/user/dashboard/page.jsx
+++ app/user/dashboard/page.jsx
@@ -1,20 +1,51 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { faHouse,faGear,faUser,faCirclePlus,faBook } from '@fortawesome/free-solid-svg-icons'
 
 function Dashboard() {
     return ( 
-        <div>
-            <h1>
-                <span className="font-bold text-3xl">Dashboard</span>                
+        <div className='bg-gray-200 h-screen'>
+            <h1 className="font-bold text-3xl text-center">
+                Dashboard    
             </h1>
-            <section>
-                <div className='flex justify-center items-center' >
-                    <FontAwesomeIcon icon={faCoffee} className='text-white h-12 w-12'/>
-                    <h2>
-                        Home
+            <div className='grid grid-cols-12 h-[calc(100vh-3rem)]'>
+                <section id='dashboard' className='bg-gray-400  h-full flex flex-col gap-y-4 p-4 col-span-2'>
+                    <div className="flex justify-start items-center gap-2 ">
+                        <FontAwesomeIcon icon={faHouse} className='text-white h-6 w-6' />
+                        <h4>
+                            Home
+                        </h4>
+                    </div>               
+                    <div className="flex justify-start items-center gap-2 ">
+                        <FontAwesomeIcon icon={faUser} className='text-white h-6 w-6' />
+                        <h4>
+                            Profile
+                        </h4>
+                    </div>               
+                    <div className="flex justify-start items-center gap-2 ">
+                        <FontAwesomeIcon icon={faGear} className='text-white h-6 w-6' />                    
+                        <h4>
+                            Settings
+                        </h4>
+                    </div>         
+                    <div className="flex justify-start items-center gap-2 ">
+                        <FontAwesomeIcon icon={faBook} className='text-white h-6 w-6' />
+                        <h4>
+                            Stories
+                        </h4>
+                    </div>               
+                    <div className="flex justify-start items-center gap-2 ">
+                        <FontAwesomeIcon icon={faCirclePlus} className='text-white h-6 w-6' />
+                        <h4>
+                            Stories
+                        </h4>
+                    </div>               
+                </section>
+                <section id="show-dashboard-items" className='bg-blue-500 col-span-10 h-full'>
+                    <h2 className='font-bold text-3xl'>
+                        Items show here
                     </h2>
-                </div>
-            </section>
+                </section>
+            </div>
         </div>
      );
 }
